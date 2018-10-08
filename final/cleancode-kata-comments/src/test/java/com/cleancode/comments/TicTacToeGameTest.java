5960316d9c82a7188cce52507c1e0d51caa14e8e
Tushar Joshi <tusharvjoshi@gmail.com>
2018-10-09 00:58:32 +0530

    Original working code

---
+++ final/cleancode-kata-comments/src/test/java/com/cleancode/comments/TicTacToeGameTest.java
@@ -0,0 +1,11 @@
+package com.cleancode.comments;
+
+import org.junit.Test;
+
+public class TicTacToeGameTest {
+
+    @Test
+    public void testGame() {
+
+    }
+}
---
+++ start/cleancode-kata-comments/README.md
@@ -0,0 +1,4 @@
+# Clean Code Kata - Comments
+
+Code used to demonstrate in a workshop how clean code principles can be
+applied to existing legacy code.
